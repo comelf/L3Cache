ad794ad2d68b0bcba04cfbbe87e1502836092842
comelf <leebyungwoo@gmail.com>
2014-12-18 05:15:34 +0900

    bug fix

--- src/test/java/controllerTest/RequestTestOfMobileAppPostsController.java
+++ src/test/java/controllerTest/RequestTestOfMobileAppPostsController.java
@@ -4,34 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.l3cache.mobileController.MobilePostsController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
-
-import util.TestUtil;
 import core.config.ApplicationConfig;
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.*;
-import static com.jayway.restassured.RestAssured.given;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=ApplicationConfig.class, loader=AnnotationConfigContextLoader.class)
--- src/test/java/controllerTest/ResultTestOfMobileAppPostsController.java
+++ src/test/java/controllerTest/ResultTestOfMobileAppPostsController.java
@@ -7,28 +7,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import util.RestAssuredUtil;
-
-import com.jayway.restassured.response.Response;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
 import core.config.ApplicationConfig;
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=ApplicationConfig.class, loader=AnnotationConfigContextLoader.class)
 public class ResultTestOfMobileAppPostsController {
 
-//	//private MockMvc mockMvc;
-//	
-//	@Autowired
-//	MobileAppPostsController mobileAppPostsControllerMock;
-//	
-
 	@Before
 	public void setup() throws Exception {
 		RestAssuredUtil.setup();
-		//RestAssuredMockMvc.standaloneSetup(new MobilePostsController());
 	}
 	
 	
@@ -36,21 +25,21 @@ public class ResultTestOfMobileAppPostsController {
 	public void test2() throws Exception {
 		//when(methodCall)
 		
-		given()
-			.param("id", "1")
-			.param("start", "1")
-			.param("sort", "0").
-        when()
-        	.post("/app/posts/").
-        then()
-        	.statusCode(200)
-        	.body("result", equalTo(10))
-        	.body("total", equalTo(51)).body("data.pid[1]", equalTo(50));
-		
-		//System.out.println(response.body());
-        	//.body("$.Response", equalTo(10));
-        	//.body();
-		
+//		given()
+//			.param("id", "1")
+//			.param("start", "1")
+//			.param("sort", "0").
+//        when()
+//        	.post("/app/posts/").
+//        then()
+//        	.statusCode(200)
+//        	.body("result", equalTo(10))
+//        	.body("total", equalTo(51)).body("data.pid[1]", equalTo(50));
+//		
+//		//System.out.println(response.body());
+//        	//.body("$.Response", equalTo(10));
+//        	//.body();
+//		
 	}
 	
 
