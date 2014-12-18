cb71d038eff79f59497d34f014c5e36ee2316d1a
comelf <leebyungwoo@gmail.com>
2014-12-18 15:12:59 +0900

    recommend test

--- src/test/java/mahoutTest/PostManagerRecommendationTest.java
+++ src/test/java/mahoutTest/PostManagerRecommendationTest.java
@@ -1,48 +1,54 @@
 package mahoutTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
-import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefItemBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
-import org.apache.mahout.cf.taste.model.JDBCDataModel;
-import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.Recommender;
-import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.l3cache.dao.PostManager;
-import org.l3cache.model.Post;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import util.RestAssuredUtil;
 import core.config.ApplicationConfig;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=ApplicationConfig.class, loader=AnnotationConfigContextLoader.class)
 public class PostManagerRecommendationTest {
 	
-	@Autowired
-	PostManager postManager;
-	
-	@Test
-	public void test() {
-		 List<Post>  list = postManager.getRecommendedLists(1, 1);
-		 for(Post item:list){
-			 System.out.println(item.getPid() +", " + item.getTitle()  );
-		 }
-		 
-		assertThat(list, notNullValue());
+	@Before
+	public void setup() throws Exception {
+		RestAssuredUtil.setup();
 	}
 	
+	@Test
+	public void recommandTest() {
+		given()
+		.param("id", "1")
+		.param("start", "1")
+		.param("sort", "0").
+    when()
+    	.post("/app/posts/").
+    then()
+    	.statusCode(200)
+    	.body("result", equalTo(10))
+    	.body("total", equalTo(4))
+    	.body("data.pid[1]", equalTo(12));
+	}
+	
+	@Test
+	public void recommandFailTest() {
+		given()
+		.param("id", "4")
+		.param("start", "1")
+		.param("sort", "0").
+    when()
+    	.post("/app/posts/").
+    then()
+    	.statusCode(200)
+    	.body("result", equalTo(10))
+    	.body("total", equalTo(0));
+	}
 	
 
 }
